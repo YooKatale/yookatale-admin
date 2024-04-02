a0882ab6d0815e64ec6385a6134451b4f41f178c
monilnathan <wamalamonil.com>
2024-04-02 14:05:09 +0300

    delivery and Vendor partner pages added on admin dashboard

---
+++ Slices/partnersPageApiSlice.js
@@ -0,0 +1,58 @@
+import { BACKEND_URL } from "@constants/constant";
+import { apiSlice } from "./apiSlice";
+
+export const partnersPageApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    partnerGet: builder.query({
+      query: () => ({
+        url: `${BACKEND_URL}/api/partners`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const {
+  usePartnerGetQuery,
+} = partnersPageApiSlice;
+
+
+
+
+
+// import { BACKEND_URL } from "@constants/constant";
+// import { apiSlice } from "./apiSlice";
+
+// export const partnersPageApiSlice = apiSlice.injectEndpoints({
+//   endpoints: (builder) => ({
+//     partnerGet: builder.query({
+//       query: () => ({
+//         url: `${BACKEND_URL}/api/partners`,
+//         method: "GET",
+//       }),
+//       async onQueryStarted(_, { dispatch, queryFulfilled, queryRejected }) {
+//         try {
+//           const response = await fetch(`${BACKEND_URL}/api/partners`, {
+//             method: 'GET',
+//             headers: {
+//               'Content-Type': 'application/json',
+//             },
+//           });
+
+//           if (!response.ok) {
+//             throw new Error('Failed to fetch partners');
+//           }
+
+//           const data = await response.json();
+//           dispatch(queryFulfilled(data));
+//         } catch (error) {
+//           dispatch(queryRejected(error));
+//         }
+//       },
+//     }),
+//   }),
+// });
+
+// export const {
+//   usePartnerGetQuery,
+// } = partnersPageApiSlice;
