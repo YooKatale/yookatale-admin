b7cf7c067e713f63a078780d544836e80a61c555
YooKatale <yookatale0@gmail.com>
2026-03-20 15:14:21 +0300

    fix: correct URL slash in applications API slice

--- Slices/applicationsApiSlice.js
+++ Slices/applicationsApiSlice.js
@@ -7,7 +7,7 @@ export const applicationsApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getApplications: builder.mutation({
       query: () => ({
-        url: `${BACKEND_URL}admin/careers/applications`,
+        url: `${BACKEND_URL}/admin/careers/applications`,
         method: "GET",
       }),
     }),
